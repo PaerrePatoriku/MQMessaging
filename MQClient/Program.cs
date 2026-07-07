7737beba1e97e8d1a88f587f45c264e2ed1162e9
Patrik Kumpula <patrik.kump@gmail.com>
2026-07-07 13:30:36 +0300

    Removing sample code

--- MQClient/Program.cs
+++ MQClient/Program.cs
@@ -16,33 +16,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.MapPost("/customs", (CustomsMessage message, ILogger<Program> logger) =>
 {
     logger.LogInformation($"Custom message received ${message.Message}, ${message.Type}. This will be queued shortly...");
 });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
